3eb763c13d9b84715ca6f5a5b0be7802fe48ef06
Mike <nhj25252@gmail.com>
2023-05-03 05:04:32 +0900

    wip

--- dist/index.js
+++ dist/index.js
@@ -2,45 +2,33 @@ array = [3, 2, 4, 0, 7, 5, 23, 12, 56, 11, 27, 68, 34, 53];
 // merge sorted arrays.
 let C = [];
 
+// A and B are sorted in order.
 function merge(A, B) {
-  // if (A.length === 1) return A;
-  // if (B.length === 1) return B;
-
   let m = A.length; // length of array A
   let n = B.length;
-  console.log(m);
-  console.log(n);
-  let i = 0;
   let k = 0;
   let j = 0;
-
-  while (i <= m && k <= n) {
-    if (A[i] < B[k]) {
-      C[j++] = A[i++];
+  let i = 0;
+  console.log(A);
+  console.log(B);
+  while (i <= m && j <= n) {
+    if (A[i] < B[j]) {
+      C[k++] = A[i++];
+      console.log(C);
     } else {
-      C[j++] = B[k++];
-      // for (i <= m; i++; ) C[j++] = A[i];
-      // for (k <= n; k++; ) C[j++] = B[k];
+      C[k++] = B[j++];
+      console.log(C);
     }
   }
-  return C;
+  return C; // return after copying all values.
 }
 
-// function merge(A,B) {
-//   const concat = A.concat(B);
-
-// // if the index has a greater number than the one after it
-// // switch
-
-// }
-
-// h is the length
 function mergeSort(array) {
   l = 0;
   h = array.length;
-  if (array.length == 1) return array;
+  if (array.length <= 1) return array;
   else if (array.length > 1) {
-    const mid = (l + h) / 2;
+    const mid = Math.ceil((l + h) / 2);
     const leftArray = array.slice(l, mid);
     const rightArray = array.slice(mid, h);
     console.log(leftArray);
@@ -49,9 +37,9 @@ function mergeSort(array) {
     console.log(sortedLeft);
     const sortedRight = mergeSort(rightArray);
     console.log(sortedRight);
-    const sortedArray = merge(sortedLeft, sortedRight);
-    // console.log(sortedArray);
-    return sortedArray;
+    const mergedArray = merge(sortedLeft, sortedRight);
+    // console.log(mergedArray);
+    return mergedArray;
   }
 }
 
